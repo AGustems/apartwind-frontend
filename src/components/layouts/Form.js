40860230b959f43e6a0e6c27029868a764ca5b35
Alba Gustems <albagustems@gmail.com>
2020-08-28 23:53:59 +0200

    Signup form completed and styled. Working with the backend

--- src/components/layouts/Form.js
+++ src/components/layouts/Form.js
@@ -5,22 +5,18 @@ const Form = (props) => {
     return (
         <div className='container-login'>
             <section className='banner-form'>
-                <div className="form-logo">
-                    <img alt="roomer-logo" src="/images/LogoRoomer-blue.png"/>
+                <div className="banner-headding">
+                    <div className="form-logo">
+                        <img alt="roomer-logo" src="/images/LogoRoomer-blue.png"/>
+                    </div>
+                    <div className="form-title">
+                        <h1>{props.title}</h1>
+                        <h3>{props.subtitle}</h3>
+                    </div>
                 </div>
-                <div className="form-title">
-                    <h1>{props.title}</h1>
-                    <h3>{props.subtitle}</h3>
-                    {props.content
-                        ? (
-                            <div clasName="banner-content">
-                                {props.content}
-                            </div>
-                        )
-                        : null}
-                </div>
+                    {props.content}
             </section>
-            <h3>{props.secondaryTitle}</h3>
+            {props.secondaryTitle ? (<h3>{props.secondaryTitle}</h3>) : null}
             {props.secondaryContent}
             <img alt="drawing" src={props.image}/> 
             {props.littleInfo}
--- src/components/views/Signup.js
+++ src/components/views/Signup.js
@@ -2,33 +2,38 @@ import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
 import axios from 'axios'
 
+import Form from '../layouts/Form'
+import InputText from '../common/InputText'
+import AgeSlider from '../common/AgeSlider'
+
+
 const Signup = (props) => {
     const initialState = {
         name: '',
         surname: '',
         occupation: '',
+        age: 0,
         email: '',
         password: '',
         description: '',
         //location: {},
+        char:'',
         characteristics: [],
         socials: {
             facebook: '',
             twitter: '',
             instagram: ''
         },
-        controls: {
-            page: 0
-        }
+        page: 0
     }
     const [userSignup,
         setUserSignup] = useState(initialState)
 
-    const handleSubmit = e => {
-        e.preventDefault()
+    const handleSubmit = () => {
         const name = userSignup.name
         const surname = userSignup.surname
         const occupation = userSignup.occupation
+        const age = userSignup.age
         const email = userSignup.email
         const password = userSignup.password
         const description = userSignup.description
@@ -40,6 +45,7 @@ const Signup = (props) => {
             name,
             surname,
             occupation,
+            age,
             email,
             password,
             description,
@@ -58,61 +64,200 @@ const Signup = (props) => {
         }))
     }
 
-    return (
-        <div className="container-login">
-            <section className="banner-form">
-                <div className="form-logo">
-                    <img alt="roomer-logo" src="/images/LogoRoomer-blue.png"/>
-                </div>
-                <div className="form-title">
-                    <h1>Hello!</h1>
-                    <h3>It's nice to meet you!</h3>
-                </div>
-            </section>
-            <form className="login-form" onSubmit={handleSubmit}>
-                <label>Name</label>
-                <input
-                    type='text'
-                    name='name'
-                    value={userSignup.name}
-                    onChange={e => handleChange(e)}/>
+    const handleChangeObject = ({target}) => {
+        setUserSignup(userSignup => ({
+            ...userSignup,
+            socials : {
+                ... userSignup.socials,
+                [target.name]:target.value
+            }
+        }))
+    }
+    
+    const changePageNext = () => {
+        const newPage = userSignup.page+1
+        setUserSignup(userSignup => ({
+            ...userSignup,
+            page : newPage
+        }))
+    }
 
-                <label>Surname</label>
-                <input
-                    type='text'
-                    name='surname'
-                    value={userSignup.surname}
-                    onChange={e => handleChange(e)}/>
+    const changePageBack = () => {
+        const newPage = userSignup.page-1
+        setUserSignup(userSignup => ({
+            ...userSignup,
+            page : newPage
+        }))
+    }
 
-                <label>Occupation</label>
-                <input
-                    type='text'
-                    name='occupation'
-                    value={userSignup.occupation}
-                    onChange={e => handleChange(e)}/>
+    const handleCharPush = (e) => {
+        e.preventDefault()
+        const charArray = userSignup.characteristics;
+        charArray.push(userSignup.char)
+        setUserSignup(userSignup => ({
+            ...userSignup,
+            characteristics: charArray,
+            char:''
+        }))
+    }
 
-                <label>Email</label>
-                <input
-                    type='text'
-                    name='email'
-                    value={userSignup.email}
-                    onChange={e => handleChange(e)}/>
-
-                <label>Password</label>
-                <input
-                    type='password'
-                    name='password'
-                    value={userSignup.password}
-                    onChange={e => handleChange(e)}/>
-
-                <input type="submit" value="Sign up"/>
-            </form>
-            <p>Already have an account?
-                <Link to={"/auth/login"}>
-                    Log in</Link>
-            </p>
+    const controllers = (
+        <div className="form-controllers">
+            <button className="faux-button" onClick={changePageBack}>Back</button>
+            <button className="faux-button" onClick={changePageNext}>Next</button>
         </div>
     )
+    
+    const showChar = userSignup.characteristics.map(char => <h4 className="title-inline">{char}</h4>)
+
+    if(userSignup.page === 0){
+        return (
+            <Form   title="Hello!"
+                    subtitle="It's nice to meet you!"
+                    secondaryTitle="Start with the sign up form"
+                    secondaryContent={
+                        <div className="register-container"><button className="register-button" onClick={changePageNext}>Registration Questions</button></div>
+                    }
+                    image="/images/signup-0.png"
+                    littleInfo = {
+                    <p>You already have an account? <Link className="w-link" to={"/auth/login"}>Log in</Link></p>
+                }
+            />
+        )
+    } else if (userSignup.page === 1){
+        return(
+            <Form   title="About you ..."
+                    subtitle="We want to know you!"
+                    content={
+                        <div className="banner-content">
+                            <InputText
+                                type="text"
+                                name="name"
+                                placeholder="Name"
+                                value={userSignup.name}
+                                onChange={e => handleChange(e)}
+                            />
+                            <InputText 
+                                type="text"
+                                name="surname"
+                                placeholder="Surname"
+                                value={userSignup.surname}
+                                onChange={e => handleChange(e)}
+                            />
+                            <InputText 
+                                type="text"
+                                name="occupation"
+                                placeholder="Occupation"
+                                value={userSignup.occupation}
+                                onChange={e => handleChange(e)}
+                            />
+                        </div>}
+                    image="/images/signup-1.png"
+                    littleInfo={controllers}
+            />
+        )
+    } else if (userSignup.page === 2){
+        return(
+            <Form   title="Age"
+                    subtitle="How old are you?"
+                    content={
+                        <div className="banner-content">
+                            <div className="age-banner">
+                                <h1>{userSignup.age}</h1>
+                            </div>
+                            <AgeSlider state={userSignup} setState={setUserSignup}/>
+                        </div>}
+                    image="/images/signup-2.png"
+                    littleInfo={controllers}
+            />)
+    } else if (userSignup.page === 3){
+        return(
+            <Form   title="Details"
+                    subtitle="Tell us about you..."
+                    content={
+                        <div>
+                            <textarea   name="description"
+                                        placeholder="Description"
+                                        rows="7"
+                                        value={userSignup.description}
+                                        onChange={e => handleChange(e)}
+                                        className="input-big"
+                            ></textarea>
+                            <form onSubmit={handleCharPush} className="char-form">
+                                <InputText  type="text"  
+                                            name="char"
+                                            placeholder="Charactetistic"
+                                            value={userSignup.char}
+                                            onChange={e => handleChange(e)}
+                                />
+                                <input type="submit" value="+" className="char-submit"/>
+                            </form>
+                            {userSignup.characteristics ? (<div className="container-char">{showChar}</div>) : null}
+                        </div>  
+                    }
+                    image="/images/signup-3.png"
+                    littleInfo={controllers}
+            />
+        )
+    } else if (userSignup.page === 4){
+        return(<Form   title="Credentials"
+                    subtitle="This is the information for the login"
+                    content={
+                        <div className="banner-content">
+                            <InputText
+                                type="text"
+                                name="email"
+                                placeholder="Email"
+                                value={userSignup.email}
+                                onChange={e => handleChange(e)}
+                            />
+                            <InputText 
+                                type="password"
+                                name="password"
+                                placeholder="Password"
+                                value={userSignup.password}
+                                onChange={e => handleChange(e)}
+                            />
+                        </div>}
+                    image="/images/signup-4.png"
+                    littleInfo={controllers}
+            />)
+    } else if (userSignup.page === 5){
+        return(
+            <Form   title="Socials"
+                    subtitle="Do you want to include your socials?"
+                    content={
+                        <div className="banner-content">
+                            <InputText
+                                type="text"
+                                name="facebook"
+                                placeholder="Facebook"
+                                value={userSignup.socials.facebook}
+                                onChange={e => handleChangeObject(e)}
+                            />
+                            <InputText 
+                                type="text"
+                                name="twitter"
+                                placeholder="Twitter"
+                                value={userSignup.socials.twitter}
+                                onChange={e => handleChangeObject(e)}
+                            />
+                            <InputText 
+                                type="text"
+                                name="instagram"
+                                placeholder="Instagram"
+                                value={userSignup.socials.instagram}
+                                onChange={e => handleChangeObject(e)}
+                            />
+                        </div>}
+                    image="/images/signup-4.png"
+                    littleInfo={<div className="submit-signup">
+                        <button className="faux-button" onClick={changePageBack}>Back</button>
+                        <button className="faux-button" onClick={handleSubmit}>Send</button>
+                    </div>}
+            />
+        )
+    }
 }
 
 export default Signup
